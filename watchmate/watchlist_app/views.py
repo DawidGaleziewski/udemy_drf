97a324cb62ab11dda4b6548513af2a8f9f56cbc7
Dawid Galeziewski <dawid.galeziewski.dev@gmail.com>
2025-11-03 19:26:35 +0100

    save notes

--- watchmate/watchlist_app/views.py
+++ watchmate/watchlist_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from urllib.request import Request
 
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Movie
 # Create your views here.
+
+
+def movie_list(request: Request):
+    movies = Movie.objects.all()
+    print(movies.values())
+    response = {
+        # we need to convert query set return ed by django into list
+        "list": list(movies.values())
+    }
+    return JsonResponse(response)
